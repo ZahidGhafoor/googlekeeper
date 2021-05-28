db870fddcef7ca78544668a8042ce8b5c94732f9
Zahid Ghafoor <zahidghafoor40@gmail.com>
2021-05-28 17:38:08 +0500

    Updates

---
+++ precache-manifest.99cef0b480606f058a143cc81368e771.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cc830304ccf9f626c5d02aca680dc9",
+    "url": "/googlekeeper/index.html"
+  },
+  {
+    "revision": "2f9f08ef7df08c0379fb",
+    "url": "/googlekeeper/static/css/main.dd5c61df.chunk.css"
+  },
+  {
+    "revision": "89d5bc9aba39d28b8eb2",
+    "url": "/googlekeeper/static/js/2.53ae956b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/googlekeeper/static/js/2.53ae956b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f9f08ef7df08c0379fb",
+    "url": "/googlekeeper/static/js/main.65ec7206.chunk.js"
+  },
+  {
+    "revision": "e40f96fe34eb58be7d90",
+    "url": "/googlekeeper/static/js/runtime-main.fb8611da.js"
+  },
+  {
+    "revision": "2a4d0e80fccc29663721d46bfeaf66c6",
+    "url": "/googlekeeper/static/media/logo.2a4d0e80.png"
+  }
+]);
